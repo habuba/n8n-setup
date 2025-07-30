2f2feaf1ccdeeda5227faa2ee6338e931d7e7bf6
habuba <habuba@gmail.com>
2025-07-31 02:14:29 +0300

    fix 310725-6

--- terrform/main.tf
+++ terrform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "~> 1.45"
+  version = "~> 1.45"
     }
   }
 }
@@ -25,3 +25,37 @@ resource "hcloud_server" "n8n" {
   user_data   = file("cloud-init.yaml")
 }
 
+
+
+
+resource "hcloud_firewall" "n8n_firewall" {
+  name = "n8n-firewall"
+
+  rule {
+    direction  = "in"
+  protocol   = "tcp"
+    port       = "22"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction  = "in"
+  protocol   = "tcp"
+    port       = "80"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction  = "in"
+  protocol   = "tcp"
+    port       = "443"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction  = "in"
+  protocol   = "tcp"
+    port       = "3002"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+}
--- terrform/outputs.tf
+++ terrform/outputs.tf
@@ -1,7 +1,3 @@
-output "server_ip" {
-  value = hcloud_server.n8n.ipv4_address
-}
-
 output "server_name" {
   value = hcloud_server.n8n.name
 }
